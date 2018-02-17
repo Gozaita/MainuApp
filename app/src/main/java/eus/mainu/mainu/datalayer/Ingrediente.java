890760e93055685fdea76548f0896422c393d5ab
Gozaita <gozaita001@ikasle.ehu.eus>
2018-02-17 13:16:48 +0100

    Fixed Adicion de data layer

--- app/src/main/java/eus/mainu/mainu/datalayer/Ingrediente.java
+++ app/src/main/java/eus/mainu/mainu/datalayer/Ingrediente.java
@@ -11,12 +11,10 @@ public class Ingrediente {
 
     private int id;
     private String nombre;
-    private ArrayList<Integer> id_bocadillos;
 
     public Ingrediente(int id, String nombre, ArrayList<Integer> id_bocadillos) {
         this.id = id;
         this.nombre = nombre;
-        this.id_bocadillos = id_bocadillos;
     }
 
     public int getId() {
@@ -34,12 +32,5 @@ public class Ingrediente {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
-    public ArrayList<Integer> getId_bocadillos() {
-        return id_bocadillos;
-    }
-
-    public void setId_bocadillos(ArrayList<Integer> id_bocadillos) {
-        this.id_bocadillos = id_bocadillos;
-    }
+    
 }
