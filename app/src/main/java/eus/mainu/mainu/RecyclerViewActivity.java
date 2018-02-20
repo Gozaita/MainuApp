1f6b3d6a2e6a22719c968a3910ae2bc59fe0f662
Gozaita <gozaita001@ikasle.ehu.eus>
2018-02-20 18:25:53 +0100

    Eliminacion de los archivos obsoletos

--- app/src/main/java/eus/mainu/mainu/RecyclerViewActivity.java
+++ app/src/main/java/eus/mainu/mainu/RecyclerViewActivity.java
@@ -17,7 +17,7 @@ public class RecyclerViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler_view);
+        //setContentView(R.layout.activity_recycler_view);
 
         initImageBitmaps();
 
