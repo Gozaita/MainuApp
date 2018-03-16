cd4f432024c9ee3fdadce23757bc3fc99c25b5b4
Gozaita <gozaita001@ikasle.ehu.eus>
2018-03-16 13:18:05 +0100

    Cambio en el layout del navigation viwer

--- app/src/main/java/eus/mainu/mainu/Activity_Main.java
+++ app/src/main/java/eus/mainu/mainu/Activity_Main.java
@@ -233,18 +233,16 @@ public class Activity_Main extends AppCompatActivity implements NavigationView.O
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.informacion) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.comparte) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.error) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.cerrarSesion) {
+            
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
